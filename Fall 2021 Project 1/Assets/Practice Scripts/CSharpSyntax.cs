96e02d530d3e1e0a5da08d85c3113d20de1274e6
Noble <nledbetter55@gmail.com>
2021-09-29 18:59:20 -0600

    C# Syntax

---
+++ Fall 2021 Project 1/Assets/Practice Scripts/CSharpSyntax.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSharpSyntax : MonoBehaviour
+    namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
