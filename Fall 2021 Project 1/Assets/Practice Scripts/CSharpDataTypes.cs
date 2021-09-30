e87a99af9d85de8be3205b67e2d85817513cde78
Noble <nledbetter55@gmail.com>
2021-09-29 19:29:55 -0600

    Added CSharpDataTypes and IntegerAndFloatingPointTypes

---
+++ Fall 2021 Project 1/Assets/Practice Scripts/CSharpDataTypes.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 5; //integers are whole numbers
+            double myDoubleNum = 5.99D; //floating point number. Note: D after number
+            char myLetter = 'D'; //Character. Note: ''
+            bool myBool = true; //true or false only
+            string myText = "HI!"; //multiple letters or words
+            Console.WriteLine(myNum);
+            Console.WriteLine(myDoubleNum);
+            Console.WriteLine(myLetter);
+            Console.WriteLine(myBool);
+            Console.WriteLine(myText);
+        }
+    }
+}
---
+++ Fall 2021 Project 1/Assets/Practice Scripts/IntegerAndFloatingPointTypes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 100000; 
+            //stores whole numbers from -2147483648 to 2147483647.
+            Console.WriteLine(myNum);
+
+            long myLongNum = 15000000000L; 
+            //stores whole numbers from -9223372036854775808 to 9223372036854775807.
+            //Note: L at end
+            Console.WriteLine(myLongNum);
+
+            float myFloatNum = 5.75F;
+            //stores fractional numbers from 3.4e−038 to 3.4e+038.
+            //Note: F at end
+            Console.WriteLine(myFloatNum);
+            //stores fractional numbers from 1.7e−308 to 1.7e+308.
+            //Note: D at end is optional
+            double myDoubleNum = 19.99D;
+
+            float f1 = 35e3F;
+            double d1 = 12E4D;
+            //Note: e or E is fine
+            Console.WriteLine(f1);
+            Console.WriteLine(d1);
+        }
+    }
+}
