979d45bed11c7ae0cabdf1daddb55d8df3e3647f
Noble <nledbetter55@gmail.com>
2021-10-01 10:06:50 -0600

    Created separate Variables and Operators exercises

---
+++ Fall 2021 Project 1/Assets/Practice Scripts/Operators.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(10 * 5);
+            Console.WriteLine(10 / 5);
+            int x = 10;
+            x++;//x=11
+            x += 5;//x=16
+        }
+    }
+}
---
+++ Fall 2021 Project 1/Assets/Practice Scripts/Variables.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 50;
+            string myName = "John";
+            int x = 5;
+            int y = 10;
+            Console.WriteLine(x + y);
+            int z = x + y;
+            Console.WriteLine(z);
+            int a = 5, b = 6, c = 50;
+            Console.WriteLine(a + b + c);
+        }
+    }
+}
