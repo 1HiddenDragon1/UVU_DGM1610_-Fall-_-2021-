7bf1dc6e1032ce84c3f861989ef8338b84ae3953
Noble <nledbetter55@gmail.com>
2021-10-04 21:05:38 -0600

    switch basic

--- Fall 2021 Project 1/Assets/Practice Scripts/Switches.cs
+++ Fall 2021 Project 1/Assets/Practice Scripts/Switches.cs
@@ -6,7 +6,32 @@ namespace MyApplication
     {
         static void Main(string[] args)
         {
-            
+            int day = 5;
+            switch(day)
+            {
+                case 1: //if the integer is 1 then this case will activate.
+                    Console.WriteLine("It is Monday");
+                    break; //the break allows the program to stop searching if this case was the one activated.
+                case 2:
+                    Console.WriteLine("It is Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("It is Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("It is Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("It is Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("It is Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("It is Sunday");
+                    break;
+                //output will be Friday (day 5)
+            }
         }
     }
 }
