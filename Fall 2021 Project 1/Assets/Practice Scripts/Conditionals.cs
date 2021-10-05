e88a52a0d0b702ba8a8e7bb5f0633ed731504991
Noble <nledbetter55@gmail.com>
2021-10-04 20:32:42 -0600

    if statements

---
+++ Fall 2021 Project 1/Assets/Practice Scripts/Conditionals.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (17 > 15)
+            {
+                Console.WriteLine("17 is greater than 15");
+            }
+
+            int x = 20;
+            int y = 15;
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
+        }
+    }
+}
