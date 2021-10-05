543df87c69db54c6822544c3d8c1cd13a2956e67
Noble <nledbetter55@gmail.com>
2021-10-04 20:45:01 -0600

    else and else if statements

--- Fall 2021 Project 1/Assets/Practice Scripts/Conditionals.cs
+++ Fall 2021 Project 1/Assets/Practice Scripts/Conditionals.cs
@@ -8,6 +8,7 @@ namespace MyApplication
         {
             if (17 > 15)
             {
+                //if statements execute this area if the "if" condition is True.
                 Console.WriteLine("17 is greater than 15");
             }
 
@@ -16,6 +17,32 @@ namespace MyApplication
             if (x > y)
             {
                 Console.WriteLine("x is greater than y");
+            }
+
+            int time = 15;
+            if (time < 12)
+            {
+                Console.WriteLine("Good Day");
+            }
+            else
+            {
+                //else statements execute this area if the "if" condition is False.
+                Console.WriteLine("Good Evening");
+            }
+            //output result is "Good Evening"
+
+            int SchoolSize = 5;
+            if (SchoolSize > 7)
+            {
+                Console.WriteLine("The school is big");
+            }
+            else if (SchoolSize < 3)
+            {
+                Console.WriteLine("The school is small");
+            }
+            else
+            {
+                Console.WriteLine("The school is quaint");
             }
         }
     }
