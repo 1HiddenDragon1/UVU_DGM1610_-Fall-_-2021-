381ad4de5f16b943259f4f49cc4af77701e530ef
Noble <nledbetter55@gmail.com>
2021-10-06 19:25:05 -0600

    added do/while loop

--- Fall 2021 Project 1/Assets/Practice Scripts/WhileLoops.cs
+++ Fall 2021 Project 1/Assets/Practice Scripts/WhileLoops.cs
@@ -12,6 +12,15 @@ namespace MyApplication
                 Console.WriteLine(i);
                 i++; //this increases the variable so that the while loop doesn't go forever.
             }
+
+            int j = 0;
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            while (j < 5);
+            //do/while loops always are executed once before they are checked to see if the conditions are met. The loop can end or continue based on the while at the end.
         }
     }
 }
