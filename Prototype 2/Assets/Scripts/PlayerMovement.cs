7b7e763737f0eb9792949fb781ea9535a9377c06
Noble <nledbetter55@gmail.com>
2021-10-26 19:58:56 -0600

    added if statement for firing food from spacebar press

--- Prototype 2/Assets/Scripts/PlayerMovement.cs
+++ Prototype 2/Assets/Scripts/PlayerMovement.cs
@@ -31,5 +31,10 @@ public class PlayerMovement : MonoBehaviour
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //will launch food projectiles from the player soon.
+        }
     }
 }
