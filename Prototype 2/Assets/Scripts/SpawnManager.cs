2e32d95ddf2e7909b59271b56b59d3ee317a3610
Noble <nledbetter55@gmail.com>
2021-10-26 21:57:11 -0600

    STAMPEEEEEED!!!!
    random spawning of animals on a timer with delay

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,20 +7,19 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] AnimalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float spawnDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", spawnDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            SpawnRandomAnimal();
-        }
+        
     }
 
     void SpawnRandomAnimal()
