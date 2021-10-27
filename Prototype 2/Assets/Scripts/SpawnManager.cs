d34d718dc810b831d6124cf79b5ece77222264be
Noble <nledbetter55@gmail.com>
2021-10-26 21:39:46 -0600

    added random area generation and cleaned up code

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] AnimalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +19,10 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             int animalIndex = Random.Range(0, AnimalPrefabs.Length);
-            Instantiate(AnimalPrefabs[animalIndex], new Vector3(0, 0, 20), AnimalPrefabs[animalIndex].transform.rotation);
+
+            Instantiate(AnimalPrefabs[animalIndex], spawnPos, AnimalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
