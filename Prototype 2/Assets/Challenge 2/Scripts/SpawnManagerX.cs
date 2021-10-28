e3d068de1ed773ca7f0ef5a27ea0e37de58197a9
Noble <nledbetter55@gmail.com>
2021-10-27 20:36:44 -0600

    made random ball from prefab group able to be instantiated

--- Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -6,9 +6,9 @@ public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] ballPrefabs;
 
-    private float spawnLimitXLeft = -22;
-    private float spawnLimitXRight = 7;
-    private float spawnPosY = 30;
+    private float spawnLimitXLeft = -22.0f;
+    private float spawnLimitXRight = 7.0f;
+    private float spawnPosY = 30.0f;
 
 
     private float startDelay = 1.0f;
@@ -25,9 +25,10 @@ public class SpawnManagerX : MonoBehaviour
     {
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 
 }
