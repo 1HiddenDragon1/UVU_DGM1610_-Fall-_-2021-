412fc8351f57fd5908e20d1f135a2feda4e41a3d
Noble <nledbetter55@gmail.com>
2021-10-27 21:40:49 -0600

    Allowed dog to spawn only every 2 seconds

--- Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float DogTimer = 2.0f;
+    private float Timer = 0f;
+        
+
 
     // Update is called once per frame
     void Update()
     {
+        Timer += Time.deltaTime;
+
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Timer > DogTimer)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Timer = 0;
         }
     }
 }
