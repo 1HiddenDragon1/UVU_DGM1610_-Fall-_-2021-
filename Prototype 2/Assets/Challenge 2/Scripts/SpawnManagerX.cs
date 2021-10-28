1ade71b66359e006e8c1c0b7a03d169013b66a50
Noble <nledbetter55@gmail.com>
2021-10-27 21:28:24 -0600

    Did bonus challenge to make random range for ball creation

--- Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,25 +10,28 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7.0f;
     private float spawnPosY = 30.0f;
 
-
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        //Generate ball at a random time between 3-5 seconds
+        float spawnInterval = Random.Range(3, 5);
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
-        // Generate random ball index and random spawn position
-        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+    // Generate random ball index and random spawn position
+    Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
         int ballIndex = Random.Range(0, ballPrefabs.Length);
 
         // instantiate ball at random spawn location
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+        
     }
 
 }
