b3f258818f987a553264b19abfdafb1f49b01615
Noble <nledbetter55@gmail.com>
2021-11-02 19:31:53 -0600

    they just keep coming!
    spawned obstacles on an invoke repeating timer

--- Prototype 3/Assets/Scripts/SpawnManager.cs
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -6,16 +6,23 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(28, 0, 0);
+    private float startDelay = 2f;
+    private float spawnTimer = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, spawnTimer);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
