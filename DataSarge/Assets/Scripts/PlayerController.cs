c1ebe8c328cebb7b8d55a3ff55ca935ebef66c8d
Noble <nledbetter55@gmail.com>
2021-11-04 21:32:12 -0600

    Roll along. Roll along.
    player movement established. I want it to have a gun it can shoot from that doesn't roll. need to figure out how to do that.

--- DataSarge/Assets/Scripts/PlayerController.cs
+++ DataSarge/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    /*speed is established for player
+     * movement is allowed for player
+     */
+    public float speed;
+    private Rigidbody playerRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //player moves at a vector 3 speed based on player input from keys
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRB.AddForce(Vector3.forward * speed * verticalInput);
+        playerRB.AddForce(Vector3.right * speed * horizontalInput);
     }
 }
