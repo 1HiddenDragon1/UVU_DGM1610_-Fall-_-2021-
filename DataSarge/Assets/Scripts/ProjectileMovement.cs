a6b6721f05270bb792c4f583dc2badb41d5a3dd0
Noble <nledbetter55@gmail.com>
2021-11-13 15:10:46 -0700

    Projectiles no longer destroy Barriers

--- DataSarge/Assets/Scripts/ProjectileMovement.cs
+++ DataSarge/Assets/Scripts/ProjectileMovement.cs
@@ -52,13 +52,4 @@ public class ProjectileMovement : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.CompareTag("Barrier"))
-        {
-            Destroy(gameObject);
-            Destroy(other.gameObject);
-        }
-    }
 }
