aa32f13b589af46403c6699d318c891d6fd9e407
Noble <nledbetter55@gmail.com>
2021-11-16 19:25:11 -0700

    Some Targets are worth more than others

--- Prototype 5/Assets/Scripts/Target.cs
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -11,6 +11,7 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -2;
     private GameManager gameManager;
+    public int pointValue;
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +33,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
