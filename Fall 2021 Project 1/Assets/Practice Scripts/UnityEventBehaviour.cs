aa7cec768090aedf3588214c1233a542fb9fe7e7
Noble <nledbetter55@gmail.com>
2021-11-30 20:35:09 -0700

    Created a basic EventBehaviour

--- Fall 2021 Project 1/Assets/Practice Scripts/UnityEventBehaviour.cs
+++ Fall 2021 Project 1/Assets/Practice Scripts/UnityEventBehaviour.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class UnityEventBehaviour : MonoBehaviour
 {
+    UnityEvent castFireball;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (castFireball == null)
+        {
+            castFireball = new UnityEvent();
+            castFireball.AddListener(Ping);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKeyDown && castFireball != null)
+        {
+            castFireball.Invoke();
+        }
+    }
+
+    void Ping()
+    {
+        Debug.Log("Ping");
     }
 }
