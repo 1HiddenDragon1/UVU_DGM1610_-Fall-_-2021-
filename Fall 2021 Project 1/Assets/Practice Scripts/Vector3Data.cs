19cd96c4e70ae8cbddc85ea3138cca05e8c6b9a9
Noble <nledbetter55@gmail.com>
2021-12-09 16:57:24 -0700

    Creating custom Vector3Data practice

--- Fall 2021 Project 1/Assets/Practice Scripts/Vector3Data.cs
+++ Fall 2021 Project 1/Assets/Practice Scripts/Vector3Data.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
-public class Vector3Data : MonoBehaviour
+[CreateAssetMenu]
+public class Vector3Data : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    public UnityEvent castSpell;
+    public float fireballSpeed;
+    public float power = 10.0f;
+
+    //In the event that UpdateFireballSpeed is started, the current fireballSpeed will be increased according to power's set amount
+    public void UpdateFireballSpeed(float power)
     {
-        
+        fireballSpeed += power;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResetFireballSpeed(float power)
     {
-        
+        fireballSpeed = power;
     }
+
+
 }
