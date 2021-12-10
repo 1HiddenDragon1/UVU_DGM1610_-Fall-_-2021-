b18ff10ffcad8295e0783fe65cb7d38bba8f7c55
Noble <nledbetter55@gmail.com>
2021-12-09 17:07:02 -0700

    added descriptive commentary and if statement to cast the fireball;

--- Fall 2021 Project 1/Assets/Practice Scripts/Vector3Data.cs
+++ Fall 2021 Project 1/Assets/Practice Scripts/Vector3Data.cs
@@ -11,16 +11,25 @@ public class Vector3Data : ScriptableObject
     public float fireballSpeed;
     public float power = 10.0f;
 
-    //In the event that UpdateFireballSpeed is started, the current fireballSpeed will be increased according to power's set amount
+    //In the event that UpdateFireballSpeed is started, the current fireballSpeed
+    //will be increased according to power's set amount
     public void UpdateFireballSpeed(float power)
     {
         fireballSpeed += power;
     }
 
+    //this can be used after the fireball has been cast or for other reasons to reset fireballSpeed
     public void ResetFireballSpeed(float power)
     {
         fireballSpeed = power;
     }
 
-
+    //if fireballSpeed reaches or exceeds 50.0f power then the fireball will be cast.
+    public void CastFireball(Image img)
+    {
+        if (fireballSpeed >= 50.0f)
+        {
+            castSpell.Invoke();
+        }
+    }
 }
